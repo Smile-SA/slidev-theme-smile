b1e8564543afdad0c0a9d0c9577cbf22be6e299c
Tony CABAYE <tony.cabaye@smile.fr>
2022-02-25 16:34:19 +0100

    feat: add 2 cols with title layout

---
+++ layouts/two-cols-with-title.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+const props = defineProps({
+  class: {
+    type: String,
+  },
+})
+</script>
+
+<template>
+  <div class="slidev-layout two-columns-with-title">
+    <slot />
+    <div class="grid grid-cols-2">
+      <div class="col-left" :class="props.class">
+        <slot name="left" />
+      </div>
+      <div class="col-right" :class="props.class">
+        <slot name="right" />
+      </div>
+    </div>
+  </div>
+</template>
