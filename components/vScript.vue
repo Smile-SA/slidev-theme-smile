c06f1e105b550a565d3c58e1270c7895c4aba90d
Tony CABAYE <tony.cabaye@smile.fr>
2022-03-24 16:00:20 +0100

    fix: only add script tag for slides that are not in overview

--- components/vScript.vue
+++ components/vScript.vue
@@ -1,8 +1,35 @@
-<script>
-import { h } from 'vue';
-export default function(props, { slots }) {
+<script setup>
+import { h, onMounted, ref, useAttrs, useSlots } from 'vue';
+
+const placeholderRef = ref();
+const isOverview = ref();
+const isMounted = ref();
+
+function hasSomeParentTheClass(element, classname) {
+  if (element.classList.contains(classname)) return true;
+  return element.parentElement && hasSomeParentTheClass(element.parentElement, classname);
+}
+
+onMounted(() => {
+  isOverview.value = hasSomeParentTheClass(placeholderRef.value, 'slides-overview');
+  isMounted.value = true;
+});
+
+const attrs = useAttrs();
+const slots = useSlots();
+
+function renderPlaceholder() {
+  return h('div', { ref: placeholderRef });
+}
+
+function renderScript() {
   if (slots.default) {
-    return h('script', props, slots.default());
+    return h('script', attrs, slots.default());
   }
 }
 </script>
+
+<template>
+  <renderPlaceholder v-if="!isMounted"></renderPlaceholder>
+  <renderScript v-if="isMounted && !isOverview"></renderScript>
+</template>
